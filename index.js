af5423bf75252bd6dc9496d9de7d7e35c109141f
Jordan Yee <jordanyee799@gmail.com>
2021-10-12 15:09:41 -0700

    commit tree fae3379b361d907616ce2948302ee1d3e8356c81

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const headquarters = 42
+const block = 264;
+
+function distanceFromHqInBlocks(dist) {
+    return Math.abs(dist - headquarters)
+}
+
+function distanceFromHqInFeet(dist) {
+    return distanceFromHqInBlocks(dist) * block;
+}
+
+function distanceTravelledInFeet(start, end) {
+    let numblocks = Math.abs(end - start);
+    return numblocks * block;
+}
+
+function calculatesFarePrice(start, end) {
+    const feetTraveled = distanceTravelledInFeet(start, end);
+    if (feetTraveled < 400) {
+        return 0;
+    } else if (feetTraveled > 400 && feetTraveled <= 2000) {
+        return (feetTraveled - 400) * 0.02;
+    } else if (feetTraveled > 2000 && feetTraveled <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
